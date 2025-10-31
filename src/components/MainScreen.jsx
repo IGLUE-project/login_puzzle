bd5f0b20f025e26251ac518df83f4207780b94ef
Javier Ferreño <javifc67@gmail.com>
2025-10-31 12:59:21 +0100

    new puzzle

---
+++ src/components/MainScreen.jsx
@@ -0,0 +1,5 @@
+import "./../assets/scss/MainScreen.scss";
+
+export default function MainScreen({ config, solved }) {
+  return <div className="frame">login</div>;
+}
---
+++ src/constants/constants.jsx
@@ -0,0 +1,21 @@
+export const DEFAULT_APP_SETTINGS = {
+  skin: "STANDARD",
+};
+
+export const THEMES = {
+  BASIC: "BASIC",
+  FUTURISTIC: "FUTURISTIC",
+  STANDARD: "STANDARD",
+  RETRO: "RETRO",
+};
+
+export const THEME_ASSETS = {
+  [THEMES.RETRO]: {},
+  [THEMES.STANDARD]: {},
+  [THEMES.FUTURISTIC]: {},
+};
+
+export const ESCAPP_CLIENT_SETTINGS = {
+  imagesPath: "./images/",
+};
+export const MAIN_SCREEN = "MAIN_SCREEN";
