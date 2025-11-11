c5e92f217749a6bec7e97a88d5952da2fca2f8d3
Clara Salgado <clara7227@adaptador-1.dit.upm.es>
2025-11-11 12:23:18 +0100

    icons added

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,19 +1,21 @@
 import "./../assets/scss/MainScreen.scss";
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
+import WifiIcon from '@mui/icons-material/Wifi';
 
 export default function MainScreen({ config, solved }) {
   return <div className="frame">
     <div className="containerLogin">
 <div className="imgAvatar"></div>
-<h2>Name of user</h2>
+<h2 className="userName">Name of user</h2>
 <input class="input" name="text" placeholder="Search..." type="search"></input>
- <p> I forgot my PIN</p>
+ <p className="forgotPIN"> I forgot my PIN</p>
  </div> 
  
  <div className="footer">
-  {/* <PowerSettingsNewIcon/> */}
-  <i>accesibilidad</i>
-  <i>login</i>
+ <WifiIcon sx={{fontSize: 20}}/>
+<AccessibilityNewIcon sx={{fontSize: 20}}/>
+   <PowerSettingsNewIcon sx={{fontSize: 20}}/>
  </div>
  </div>;
 }
